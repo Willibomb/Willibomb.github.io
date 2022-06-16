6b609fb8a0c9dc6104c450c895e9dfa2576f686f
Willibomb <94479851+Willibomb@users.noreply.github.com>
2022-06-16 15:11:14 -0700

    Add files via upload

--- William-FactFinder/script.js
+++ William-FactFinder/script.js
@@ -5,6 +5,8 @@ function factA(){
 	document.querySelector("main").style.backgroundColor = "#FF55AA";
 	document.querySelector("#factdisplay").style.color = "#177013";
 	document.querySelector("form").style.backgroundColor = "#DF5678";
+	document.querySelector("main").style.border = "5px solid #67459F";
+	document.querySelector("#factdisplay").style.border = "5px dotted #650D1B";
 }
 factForm.b.addEventListener("click",factB);
 function factB(){
@@ -12,6 +14,8 @@ function factB(){
 	document.querySelector("main").style.backgroundColor = "#C8A353";
 	document.querySelector("#factdisplay").style.color = "#45EF90";
 	document.querySelector("form").style.backgroundColor = "#989898";
+	document.querySelector("main").style.border = "5px solid #11FFFF";
+	document.querySelector("#factdisplay").style.border = "5px dotted #823200";
 }
 factForm.c.addEventListener("click",factC);
 function factC(){
@@ -19,6 +23,8 @@ function factC(){
 	document.querySelector("main").style.backgroundColor = "#EEB4B4";
 	document.querySelector("#factdisplay").style.color = "#4B4BEE";
 	document.querySelector("form").style.backgroundColor = "#555555";
+	document.querySelector("main").style.border = "5px solid #567891";
+	document.querySelector("#factdisplay").style.border = "5px dotted #9B3D12";
 }
 factForm.d.addEventListener("click",factD);
 function factD(){
@@ -26,6 +32,8 @@ function factD(){
 	document.querySelector("main").style.backgroundColor = "#A9FDAC";
 	document.querySelector("#factdisplay").style.color = "#FB4578";
 	document.querySelector("form").style.backgroundColor = "#febbcc";
+	document.querySelector("main").style.border = "5px solid #3489FF";
+	document.querySelector("#factdisplay").style.border = "5px dotted #AE8E1C";
 }
 factForm.e.addEventListener("click",factE);
 function factE(){
@@ -33,4 +41,6 @@ function factE(){
 	document.querySelector("main").style.backgroundColor = "#B8336A";
 	document.querySelector("#factdisplay").style.color = "#DDDDFF";
 	document.querySelector("form").style.backgroundColor = "#7856ab";
+	document.querySelector("main").style.border = "5px solid #65FF11";
+	document.querySelector("#factdisplay").style.border = "5px dotted #C1DF1F";
 }
